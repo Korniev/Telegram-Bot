6b8b8f45f37a9e6e14357746960bfc074ef88335
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2022-02-27 21:55:50 +0100

    Config-file
    There is a bot's token.

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5135192177:AAEaQh-xRvqxHGG8jxniON8iKfDqBDwI_Pw"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'гривна': 'UAH',
+    'биткоин' : 'BTC',
+    'этэриум' : 'ETH',
+    'доджкоин' : 'DOGE'
+}
